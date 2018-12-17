8d3510941362772a5b368702ece1cf9c6ff20018
JeremyGuan <silence-23@hotmail.com>
2018-12-17 17:35:43 +0800

    Site updated: 2018-12-17 17:35:43

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
 ], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
